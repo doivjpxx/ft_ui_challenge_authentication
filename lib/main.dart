3d426762172fff8b97a785b4c43382d6ca3b27a7
Huy Phong <doivjpxx@gmail.com>
2021-01-23 12:09:09 +0700

    completed authenticatio 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fl_ui_challenge_authentication/pages/auth_1.dart';
 import 'package:fl_ui_challenge_authentication/pages/auth_2.dart';
+import 'package:fl_ui_challenge_authentication/pages/auth_3.dart';
 import 'package:flutter/material.dart';
 
 import 'pages/home_page.dart';
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         '/': (context) => MyHomePage(),
         '/first': (context) => MyAuthentication1(),
         '/second': (context) => MyAuthentication2(),
-        // '/third': (context) => MyAuthentication3(),
+        '/third': (context) => MyAuthentication3(),
         // '/four': (context) => MyAuthentication4(),
         // '/five': (context) => MyAuthentication5(),
         // '/six': (context) => MyAuthentication6(),
